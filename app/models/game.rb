0b8ffa578e3d5479180bcb1078cff0d5e5fb9e94
dariusz gorzeba <darekg@innov8on.com>
2012-08-25 13:48:22 +0200

    models: turn and choise

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,13 @@
 class Game
   attr_accessor :name, :users
+  attr_accessor :turns
+
+  TURN_LIMIT = 10
 
   def initialize(name)
     @name = name
     @users = []
+    @turns = Array.new(TURN_LIMIT).fill(Turn.new)
   end
 
   def process
@@ -14,8 +18,41 @@ class Game
     self.users.size < 2
   end
 
-
   def to_param
     self.name
+  end
+
+  def finished?
+    false
+  end
+
+  class Turn
+    attr_accessor :choises
+    def initialize
+      @choises = [Choise.new,Choise.new]
+    end
+
+    def completed?
+      @choises.map(&:done?) == [true,true]
+    end
+
+    def result
+      raise 'round_incomplete' unless completed?
+
+      @choises[0] == @choises[1]
+    end
+  end
+
+  class Choise
+    VALUES = (0..15).to_a
+    attr_accessor :value
+
+    def done?
+      !!value
+    end
+
+    def ==(other)
+      self.value == other.value
+    end
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -12,5 +12,33 @@ describe Game do
     }.to raise_error('missing_player')
   end
 
+  context ' new game state ' do
+    its(:turns) { should have(Game::TURN_LIMIT).rounds }
+  end
 
+  describe Game::Turn do
+    it ' is incomplete after init' do
+      Game::Turn.new.should_not be_completed
+    end
+    it ' turn ' do
+      turn = Game::Turn.new
+
+      expect{
+        turn.result
+      }.to raise_error('round_incomplete')
+
+      turn.choises[0].value = 1
+      turn.choises[1].value = 1
+      turn.result.should be_true
+
+      turn.choises[0].value = 2
+      turn.choises[1].value = 5
+      turn.result.should be_false
+
+      turn.choises[0].value = 1
+      turn.choises[1].value = 0
+      turn.result.should be_false
+
+    end
+  end
 end
